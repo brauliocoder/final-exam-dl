40e69fbbdb469f7599c65d900b2781fe40c9416c
Braulio <b.oyarzun.barros@gmail.com>
2022-05-11 18:23:10 -0400

    total income by headquarter

--- app/models/headquarter.rb
+++ app/models/headquarter.rb
@@ -5,4 +5,16 @@ class Headquarter < ApplicationRecord
   has_many :channels, dependent: :destroy
   has_many :memberships, dependent: :destroy
   has_and_belongs_to_many :warehouses
+
+  def total_income
+    total = 0
+    
+    self.channels.each do |channel|
+      channel.orders.each do |order|
+        total += order.total_order
+      end
+    end
+    
+    return total
+  end
 end
